b01b445eaa033c6cbd7dfbdbb0c4678750d93a9a
James <jaimea111@gmail.com>
2018-10-16 21:56:29 -0500

    Imagenes del laboratorio

--- src/main/java/codigoLaboratorio/servidor.java
+++ src/main/java/codigoLaboratorio/servidor.java
@@ -4,11 +4,14 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.math.BigInteger;
+import java.net.InetAddress;
 
 public class servidor{
     public static void main(String[] args) throws IOException {
         //Esta será Alice...
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        InetAddress inetAddress = InetAddress.getLocalHost();
 
         System.out.println("Introduzca el numero primo:");
         BigInteger modulo=new BigInteger(br.readLine());
@@ -22,11 +25,15 @@ public class servidor{
         generadorClave generador = new generadorClave(coeficiente,exponente,modulo);
         BigInteger R1 = generador.generarClave();
 
+        System.out.println("IP Address:- " + inetAddress.getHostAddress());
+        System.out.println("Host Name:- " + inetAddress.getHostName());
+
         enviarInfo(R1, 4000);
 
     }
 
     public static void enviarInfo(BigInteger datos, int puerto)  throws IOException {
+        System.out.println();
         ServerSocket ss = new ServerSocket(4000);
         Socket socket = ss.accept();
         OutputStream out = socket.getOutputStream();
